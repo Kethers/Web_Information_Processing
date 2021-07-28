9c76919ae516821a6417f525e1fd40470eec6587
Kethers <49612587+Kethers@users.noreply.github.com>
2021-07-28 17:56:09 +0800

    Add files via upload

---
+++ 3/project3/imdb_lstm_dropout.py
@@ -0,0 +1,25 @@
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+# 加载数据集
+numpy.random.seed(7)
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=5000)
+# 设置最大输出序列长度
+outputlen = 500
+X_train = sequence.pad_sequences(X_train, maxlen=outputlen)
+X_test = sequence.pad_sequences(X_test, maxlen=outputlen)
+# 设置模型参数并进行训练
+
+testmodel = Sequential()
+testmodel.add(Embedding(5000, 32, input_length=outputlen))
+testmodel.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+testmodel.add(Dense(1, activation='sigmoid'))
+testmodel.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(testmodel.summary())
+testmodel.fit(X_train, y_train, epochs=3, batch_size=64)
+# 用测试集测试得到结果并输出
+scores = testmodel.evaluate(X_test, y_test, verbose=0)
+print("准确度: %.2f%%" % (scores[1]*100))
